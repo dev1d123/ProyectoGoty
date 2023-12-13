2f7d6094b28d30b57b64925af7214229c89d1835
gonzaloSaavedra17 <gsaavedrai@unsa.edu.pe>
2023-12-13 15:24:55 -0500

    Clase Unit (Unidad) creada

---
+++ Unit.java
@@ -0,0 +1,55 @@
+public class Unit{
+    private int vida;
+    private int ataque;
+    private int velocidad;
+    private int pp;
+    private String habilidad;
+    
+    public Unit(int vida, int ataque, int velocidad, int pp, String habilidad) {
+        this.vida = vida;
+        this.ataque = ataque;
+        this.velocidad = velocidad;
+        this.pp = pp;
+        this.habilidad = habilidad;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int getPp() {
+        return pp;
+    }
+
+    public void setPp(int pp) {
+        this.pp = pp;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(String habilidad) {
+        this.habilidad = habilidad;
+    }
+}
